12347b292c831763bf14070c0ece30959b533501
aleegaarza <aleegaarza@gmail.com>
2023-04-01 19:56:28 -0600

    added header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <div className="container">
+        <a className="navbar-brand" href="/">
+          react app
+        </a>
+      </div>
+    </nav>
+  );
+};
